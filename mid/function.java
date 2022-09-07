931f32ab1dd4b4f5db800a982e85360a3fcf8124
Damber Bokati <damberbokati@Dambers-MacBook-Pro.local>
2022-09-07 14:49:52 -0500

    First commit

---
+++ mid/function.java
@@ -0,0 +1,16 @@
+public class function {
+
+    public static void main(String[] args) {
+
+        String a = get("Damber");
+
+        System.out.println(a);
+    }
+
+    public static String get(String s) {
+
+        return s + "!";
+
+    }
+
+}
---
+++ mid/multi.java
@@ -0,0 +1,8 @@
+public class multi {
+
+    public static void main(String[] args) {
+
+        System.out.println("%30s Table 12 Times\n\n");
+    }
+
+}
