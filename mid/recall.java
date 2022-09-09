17f3408beeaf62b5f230b7c27171f9e342f752b3
Damber Bokati <damberbokati@Dambers-MacBook-Pro.local>
2022-09-08 20:27:44 -0500

    Adding trim and recall file

---
+++ mid/recall.java
@@ -0,0 +1,15 @@
+public class recall {
+
+    public static void main(String[] args) {
+
+        System.out.printf("\n\n%25s Times 12 Table\n\n", " ");
+
+        System.out.println("     ");
+
+        for(int i=0; i<=12;++i){
+
+            System.out.println("");
+            
+        }
+    }
+}
---
+++ mid/trim.java
@@ -0,0 +1,10 @@
+public class trim {
+
+    public static void main(String[] args) {
+
+        String Dam = " I am Damber!  ";
+
+        System.out.println(Dam.trim() + Dam.trim().toUpperCase());
+    }
+
+}
