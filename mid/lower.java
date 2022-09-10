c5eaee54c5d182a7aa5ab5bda8c3139efb60501b
Damber Bokati <damberbokati@Dambers-MacBook-Pro.local>
2022-09-09 22:57:24 -0500

    Lower case program java

---
+++ mid/lower.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class lower {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String Name = in.nextLine();
+
+        System.out.println(Name.toLowerCase());
+
+    }
+
+}
